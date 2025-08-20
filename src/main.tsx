332a2c008faf97cc9b956118dfb3826ae377541d
Akerman048 <akkerman048@gmail.com>
2025-08-20 21:20:28 +0000

    Fix TS6133 unused vars/imports before deploy

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,10 @@ import "./index.css";
 import App from "./App.tsx";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./routes/HomePage/HomePage.tsx";
 import Project from "./routes/Project/Project.tsx";
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
